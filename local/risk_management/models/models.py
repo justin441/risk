7d93a17e9f4452cf38de5c907edc06ad9b4cb2ab
Fotue Kamga Justin <fotuejustin441@gmail.com>
2019-03-27 17:50:36 +0100

    added `Process` and `ProcessData` models

--- local/risk_management/models/models.py
+++ local/risk_management/models/models.py
@@ -2,14 +2,43 @@
 
 from odoo import models, fields, api
 
-# class risk_management(models.Model):
-#     _name = 'risk_management.risk_management'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class Process(models.Model):
+    _name = 'risk_management.process'
+    _description = 'A business process'
+    _sql_constraints = [
+        ('process_name_unique',
+         'UNIQUE(name)',
+         'The process name must be unique.')
+    ]
+    name = fields.Char(required=True, index=True, translate=True, size=255)
+    process_type = fields.Selection(selection=[('O', 'Operation'), ('M', 'Management'), ('S', 'Support')], default='O')
+    description = fields.Text(required=True, translate=True)
+    responsible_id = fields.Many2one('res.user', ondelete='set null', delegate=True, string='Responsible')
+    output_data_ids = fields.One2many('risk_management.process_data', inverse_name='int_provider_id',
+                                      string='Output data')
+    input_data_ids = fields.Many2many(comodel_name='risk_management.process_data',
+                                      relation='risk_management_input_ids_consumers_ids_rel',
+                                      column1='input_data_ids', column2='consumer_ids', string="Input data",
+                                      domain="[('int_provider_id.name', '!=', 'name')]")
+
+
+class ProcessData(models.Model):
+    _name = 'risk_management.process_data'
+    _description = 'Process input or output'
+    _sql_constraints = [
+        (
+            'data_name_unique',
+            'UNIQUE(name)',
+            'The process data name must be unique'
+        )
+    ]
+
+    name = fields.Char(required=True, index=True, translate=True, size=255)
+    ext_provider_id = fields.Many2one('res.partner', string='External provider', ondelete='cascade')
+    int_provider_id = fields.Many2one('risk_management.process', string='Internal provider', ondelete='cascade',
+                                      delegate=True)
+    consumer_ids = fields.Many2many(comodel_name='risk_management.process',
+                                    relation='risk_management_input_ids_consumers_ids_rel',
+                                    column1='consumer_ids', column2='input_data_ids',
+                                    domain="[('name', '!=', 'int_provider_id.name')]")
