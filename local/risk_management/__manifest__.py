0133fd2b100b7f40da2a3c0bde4ae006aeba8991
Fotue Kamga Justin <fotuejustin441@gmail.com>
2019-03-27 11:43:33 +0100

    Manifest 1.0

--- local/risk_management/__manifest__.py
+++ local/risk_management/__manifest__.py
@@ -1,26 +1,28 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "risk_management",
+    'name': "Risk management",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+    Risk management, Activity, Process
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+    Risk management
+    ===============
+    
+    Add risks to a business processes and manage said risks.
+    """,
+
+    'author': "Noubru Holding",
+    'website': "http://noubruholding.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    'category': 'Risk management',
+    'version': '11.0.0.1',
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'project'],
 
     # always loaded
     'data': [
@@ -32,4 +34,8 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    
 }
