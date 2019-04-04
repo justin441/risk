a5b07ef70d04c9cb3f867edd69f10fd6683941f7
Fotue Kamga Justin <fotuejustin441@gmail.com>
2019-04-04 18:02:15 +0100

    bugs correction

--- local/risk_management/models/partner_category.py
+++ local/risk_management/models/partner_category.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 
 class PartnerCategory(models.Model):
     """Add output data to partner category"""
-    _inherits = 'res.partner.category'
+    _inherit = 'res.partner.category'
 
     output_data_ids = fields.One2many('risk_management.process_data', inverse_name='ext_provider_cat_id',
                                       string="Output data")
