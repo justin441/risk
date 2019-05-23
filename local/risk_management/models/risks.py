15f35893805ab297a1c1d031d3e305c6f4d220f0
Justin Fotue Kamga <fotuejustin441@gmail.com>
2019-05-23 16:33:13 +0100

    [CHANGE] added risks file

--- local/risk_management/models/risks.py
+++ local/risk_management/models/risks.py
@@ -1,1 +1,14 @@
-from odoo import fields
+from odoo import models, fields, api
+
+
+class RiskClass(models.Model):
+    _name = 'risk_management.risk_class'
+
+    name = fields.Char(translate=True)
+
+
+class BaseRisk(models.AbstractModel):
+    _name = 'risk_management.base_risk'
+
+    short_name = fields.Char(translate=True)
+    name = fields.Char(translate=True, index=True)
