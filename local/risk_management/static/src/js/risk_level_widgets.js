659be21ef6bf9aee86e187cfdb52d9875e7eb823
Justin Fotue Kamga <fotuejustin441@gmail.com>
2019-07-11 17:52:22 +0100

    [FIX] The widget color depends from the type of the risk

--- local/risk_management/static/src/js/risk_level_widgets.js
+++ local/risk_management/static/src/js/risk_level_widgets.js
@@ -8,7 +8,6 @@ odoo.define('risk_management.progress_bar', function (require) {
 
     ProgressBar.include({
         init: function () {
-            console.log('From my widget');
             this._super.apply(this, arguments);
 
             // the progressbar needs the values and not the field name, passed in options
@@ -19,6 +18,7 @@ odoo.define('risk_management.progress_bar', function (require) {
 
             // the ref_value is the value that the widget uses to determine its color
             this.ref_value = this.recordData[this.nodeOptions.ref_value] || 100;
+            this.risk_type = this.recordData['risk_type'] || false;
             this.readonly = this.nodeOptions.readonly || !this.nodeOptions.editable;
             this.edit_max_value = this.nodeOptions.edit_max_value || false;
             this.title = _t(this.attrs.title || this.nodeOptions.title) || '';
@@ -30,7 +30,8 @@ odoo.define('risk_management.progress_bar', function (require) {
         _render_value: function (v) {
             var value = this.value;
             var max_value = this.max_value;
-            var ref_value = this.ref_value
+            var ref_value = this.ref_value;
+            var risk_type = this.risk_type
             if (!isNaN(v)) {
                 if (this.edit_max_value) {
                     max_value = v;
@@ -49,13 +50,19 @@ odoo.define('risk_management.progress_bar', function (require) {
             }
 
             this.$('.o_progress').toggleClass('o_progress_overflow', value > max_value);
-            this.$('.o_progressbar_complete').toggleClass('o_progress_acceptable', value < ref_value).css('width', widthComplete + '%');
-            this.$('.o_progressbar_complete').toggleClass('o_progress_unacceptable', value > ref_value).css('width', widthComplete + '%');
+            if (risk_type === 'O') {
+                this.$('.o_progressbar_complete').toggleClass('o_progress_acceptable', value > ref_value).css('width', widthComplete + '%');
+                this.$('.o_progressbar_complete').toggleClass('o_progress_unacceptable', value < ref_value).css('width', widthComplete + '%');
+            }
+            else {
+                this.$('.o_progressbar_complete').toggleClass('o_progress_acceptable', value < ref_value).css('width', widthComplete + '%');
+                this.$('.o_progressbar_complete').toggleClass('o_progress_unacceptable', value > ref_value).css('width', widthComplete + '%');
+            }
             this.$('.o_progressbar_complete').css('width', widthComplete + '%');
 
             if (!this.write_mode) {
                 if (max_value !== 100) {
-                    this.$('.o_progressbar_value').text(utils.human_number(value));
+                    this.$('.o_progressbar_value').text(utils.human_number(value) + " / " + utils.human_number(max_value));
                 } else {
                     this.$('.o_progressbar_value').text(utils.human_number(value) + "%");
                 }
