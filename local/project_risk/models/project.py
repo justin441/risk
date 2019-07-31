e16d7c77e4a2d07ee168652910ad4a27d595da38
Justin Fotue Kamga <fotuejustin441@gmail.com>
2019-07-31 14:04:54 +0100

    [CHANGE] deleted 2 methods on project models.

--- local/project_risk/models/project.py
+++ local/project_risk/models/project.py
@@ -4,32 +4,8 @@ from odoo import models, fields, api
 class Project(models.Model):
     _inherit = 'project.project'
 
-    risk_ids = fields.One2many('project_risk.risk', inverse_name='project_id', string='Risks')
+    risk_ids = fields.One2many('project_risk.project_risk', inverse_name='project_id', string='Risks')
     risk_count = fields.Integer('Active risks', compute='_compute_active_risks')
-
-    @api.multi
-    def _compute_active_risks(self):
-        for rec in self:
-            rec.risk_count = len(rec.risk_ids.filtered('active'))
-
-    @api.multi
-    def get_risk_treatment_task(self):
-        self.ensure_one()
-        task = self.env['project.task']
-        risk_treatment_task = task.search([('name', '=', '%s / Risk Treatment' % self.name),
-                                           ('project_id', '=', self.id)])
-        if not risk_treatment_task.exists():
-            risk_treatment_task = task.create({
-                'name': '%s / Risk Treatment' % self.name,
-                'description': """
-                <p>The purpose of this task is to select and implement measures to modify the risks. 
-                    These measures can include avoiding, optimizing, transferring or retaining risk</p>
-                """,
-                'priority': 1,
-                'sequence': 1,
-                'project_id': self.id,
-            })
-        return risk_treatment_task
 
     @api.multi
     def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None, force=True):
@@ -53,6 +29,6 @@ class Project(models.Model):
 class Task(models.Model):
     _inherit = 'project.task'
 
-    project_risk_ids = fields.Many2many('project_risk.risk', relation='project_risk_task_ids_risk_ids_rel',
-                                        column1='project_risk_ids', column2='task_ids', string='Risks')
-    project_risk_id = fields.Many2one('project_risk.risk', string='Risk')
+    project_risk_ids = fields.Many2many('project_risk.project_risk', relation='project_risk_task_ids_risk_ids_rel',
+                                        column1='task_id', column2='project_risk_id', string='Risks')
+    project_risk_id = fields.Many2one('project_risk.project_risk', string='Treated Risk')
