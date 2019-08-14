6a8aac532957fe8368d26d6e4fa5a781f236d71b
Justin Fotue Kamga <fotuejustin441@gmail.com>
2019-08-14 16:36:02 +0100

    [CHANGE] Structural changes

--- local/risk_management/tests/test_process.py
+++
@@ -1,6 +0,0 @@
-from odoo.tests.common import TransactionCase
-
-
-class TestProcess(TransactionCase):
-    def setUp(self):
-        super(TestProcess, self).setUp()
--- local/risk_management/tests/test_process_data.py
+++
@@ -1,7 +0,0 @@
-from odoo.tests.common import TransactionCase
-
-
-class TestProcessData(TransactionCase):
-    def setUp(self):
-        super(TestProcessData, self).setUp()
-        pass
