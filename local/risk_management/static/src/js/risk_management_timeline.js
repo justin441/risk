954057f06b124b57d7c4c404bdc61664eb012dad
Justin Fotue Kamga <fotuejustin441@gmail.com>
2019-10-23 15:31:13 +0100

    [CHANGE] Use Chartjs instead of nvd3

--- local/risk_management/static/src/js/risk_management_timeline.js
+++ local/risk_management/static/src/js/risk_management_timeline.js
@@ -1,14 +1,33 @@
 odoo.define('risk_management.timeline_graph', function (require) {
-   'use strict';
-   require('web.dom_ready');
-   var ajax = require('web.ajax');
-   var risk_ids = $('#risk_ids').data('risk');
-   return ajax.rpc('/web/dataset/call', {
-                model:  'risk_management.business_risk.evaluation',
-                method: 'search_read',
-                args: [['&', ['business_risk_id', 'in', risk_ids], ['is_valid', '=', 1]]]
-            }).then(function (data) {     
-            console.log(data);
-            console.log(d3.time.format('%x')(new Date(1251691200000)));
+    'use strict';
+    require('web.dom_ready');
+    var ajax = require('web.ajax');
+    var risk_ids = $('#risk_ids').data('risk');
+
+    var render_chart = function (risks) {
+        console.log(risks);
+    }
+    return ajax.rpc('/web/dataset/call', {
+        model: 'risk_management.business_risk.evaluation',
+        method: 'search_read',
+        args: [
+            ['&', ['business_risk_id', 'in', risk_ids], ['is_valid', '=', 1]],
+            ['business_risk_id', 'eval_date', 'detectability', 'occurrence', 'severity', 'value',
+                'threshold_detectability', 'threshold_occurrence', 'threshold_severity', 'threshold_value']
+        ]
+    }).then(function (eval_data) {
+        var risk_data = [];
+        risk_ids.forEach(function (risk_id) {
+            var risk = {id: risk_id, evaluations: []};
+            eval_data.forEach(function (evaluation) {
+                evaluation.eval_date = new Date(evaluation.eval_date + "Z");
+                if (evaluation.business_risk_id[0] === risk_id) {
+                    risk.evaluations.push(evaluation);
+                }
             });
+            risk_data.push(risk);
+        });
+        render_chart(risk_data);
+
+    });
 });
