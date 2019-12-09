e38e89c08c13c99139c5f3612b4ca2ca6c425cd5
Justin Fotue Kamga <fotuejustin441@gmail.com>
2019-12-09 10:43:21 +0100

    [CHANGE] wkhtmltopdf crash fix

---
+++ local/risk_management/static/src/js/pdf_render_polyfill.js
@@ -0,0 +1,20 @@
+odoo.define('risk_management.pdf_polyfill', function (require) {
+    // wkhtmltopdf 0.12.5 crash fix.
+    // https://github.com/wkhtmltopdf/wkhtmltopdf/issues/3242#issuecomment-518099192
+    'use strict';
+    (function (setLineDash) {
+        CanvasRenderingContext2D.prototype.setLineDash = function () {
+            if (!arguments[0].length) {
+                arguments[0] = [1, 0];
+            }
+            // Now, call the original method
+            return setLineDash.apply(this, arguments);
+        };
+    })(CanvasRenderingContext2D.prototype.setLineDash);
+    Function.prototype.bind = Function.prototype.bind || function (thisp) {
+        var fn = this;
+        return function () {
+            return fn.apply(thisp, arguments);
+        };
+    };
+});
